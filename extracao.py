c813121c5207cab0e693b5a83b504af29d396bad
Filipe Ribeiro <liperrocha@gmail.com>
2024-06-15 15:15:22 -0300

    analise estatistica

--- extracao.py
+++ extracao.py
@@ -3,71 +3,98 @@ import pandas as pd
 import matplotlib.pyplot as plt
 import seaborn as sns
 
-# Load JSON data from files
-with open('legos_amazon_brasil.json', 'r', encoding='utf-8') as f:
-    amazon_brasil_data = json.load(f)
+# Leitura dos JSON
+with open('produto_menor_preco.json', 'r', encoding='utf-8') as f:
+    menor_preco_data = json.load(f)
 
-with open('legos_bricklink.json', 'r', encoding='utf-8') as f:
-    bricklink_data = json.load(f)
+# Convertendo JSON para DataFrames
+df_menor_preco = pd.DataFrame(menor_preco_data)
 
-with open('legos_lego_store_brasil.json', 'r', encoding='utf-8') as f:
-    lego_store_brasil_data = json.load(f)
+# Remover caracteres indesejados e converter para tipo numérico
+def clean_and_convert(df, price_col, pieces_col):
+    df[price_col] = df[price_col].replace('[\$,]', '', regex=True).astype(float)
+    df[pieces_col] = pd.to_numeric(df[pieces_col], errors='coerce')
+    return df
 
-# Convert JSON data to DataFrames
-df_amazon_brasil = pd.DataFrame(amazon_brasil_data)
-df_bricklink = pd.DataFrame(bricklink_data)
-df_lego_store_brasil = pd.DataFrame(lego_store_brasil_data)
+df_menor_preco = clean_and_convert(df_menor_preco, 'Preco', 'Num Pecas')
 
-# Add a source column to each DataFrame
-df_amazon_brasil['Loja'] = 'Amazon Brasil'
-df_bricklink['Loja'] = 'Bricklink'
-df_lego_store_brasil['Loja'] = 'Lego Store Brasil'
+# Garantir que os indices sejam unicos
+df_all = df_menor_preco.reset_index(drop=True)
 
+# Definindo as cores
+color = [(0.99, 0.64, 0.13, 1.0),(0.83, 0.42, 0.23, 1.0), (0.86, 0.28, 0.39, 1.0)]
 
-
-# Concatenate DataFrames into a single DataFrame
-df_all = pd.concat([df_amazon_brasil, df_bricklink, df_lego_store_brasil])
-
-# Display basic information about the combined DataFrame
-df_all.info(), df_all.head()
-
-# 1. Gráfico de Barras: Preço Médio por Loja
+# 1. Grafico de Barras: Preço Medio por Loja
 preco_medio_por_loja = df_all.groupby('Loja')['Preco'].mean()
-
 plt.figure(figsize=(10, 6))
-sns.barplot(x=preco_medio_por_loja.index, y=preco_medio_por_loja.values)
+plt.grid(axis = 'y', linestyle = '--')
+plt.bar(preco_medio_por_loja.index, preco_medio_por_loja.values, color=color)
 plt.title('Preço Médio por Loja')
 plt.xlabel('Loja')
 plt.ylabel('Preço Médio (R$)')
-plt.xticks(rotation=45)
-plt.show()
 
-# 2. Gráfico de Pizza: Distribuição de Produtos por Loja
+# 2. Grafico de Pizza: Distribuicao de Produtos por Loja
 distribuicao_por_loja = df_all['Loja'].value_counts()
 
 plt.figure(figsize=(8, 8))
 plt.pie(distribuicao_por_loja, labels=distribuicao_por_loja.index, autopct='%1.1f%%', startangle=140)
 plt.title('Distribuição de Produtos por Loja')
-plt.show()
 
-# 3. Histograma: Variação de Preços
-plt.figure(figsize=(10, 6))
+# 3. Histogramas
+plt.figure(figsize=(14, 6))
+# Histograma de Preços
+plt.subplot(1, 2, 1)
+plt.grid(axis = 'y', linestyle = '--')
 sns.histplot(df_all['Preco'].dropna(), kde=True)
-plt.title('Variação de Preços dos Produtos Lego')
+plt.title('Distribuição de Preços')
 plt.xlabel('Preço (R$)')
 plt.ylabel('Frequência')
-plt.show()
 
-# 4. Estatísticas Descritivas: Preços
-estatisticas_descritivas = df_all.groupby('Loja')['Preco'].describe()
+# Histograma de Numero de Peças
+plt.subplot(1, 2, 2)
+plt.grid(axis = 'y', linestyle = '--')
+sns.histplot(df_all['Num Pecas'].dropna(), kde=True)
+plt.title('Distribuição de Número de Peças')
+plt.xlabel('Número de Peças')
+plt.ylabel('Frequência')
+
+plt.tight_layout()
+
+# 4. Grafico com o Preco Minimo e Maximo
+# Calcular a media, minimo e maximo dos preços por loja
+price_stats_by_store = df_all.groupby('Loja')['Preco'].agg(['mean', 'min', 'max']).reset_index()
+
+# Plotar o preço minimo e maximo por loja
+plt.figure(figsize=(10, 6))
+plt.grid(axis = 'y', linestyle = '--')
+sns.lineplot(data=price_stats_by_store, x='Loja', y='min', label='Mínimo', marker='o')
+sns.lineplot(data=price_stats_by_store, x='Loja', y='max', label='Máximo', marker='o')
+plt.title('Preço Mínimo e Máximo por Loja')
+plt.xlabel('Loja')
+plt.ylabel('Preço (R$)')
+plt.legend()
+
+# 5. Grafico Preço vs Numero de Peças
+plt.figure(figsize=(8, 6))
+plt.grid(axis = 'y', linestyle = '--')
+sns.scatterplot(data=df_all, x='Num Pecas', y='Preco')
+plt.title('Relação entre Preço e Número de Peças')
+plt.xlabel('Número de Peças ')
+plt.ylabel('Preço (R$) ')
+
+# 6. Boxplot dos preços por loja
+plt.figure(figsize=(10, 6))
+plt.grid(axis = 'y', linestyle = '--')
+sns.boxplot(data=df_all, x='Loja', y='Preco')
+plt.title('Distribuição dos Preços por Loja')
+plt.xlabel('Loja')
+plt.ylabel('Preço (R$)')
+# plt.xticks(rotation=45)
+
+# 7. Estatisticas Descritivas
+# Precos
+estatisticas_descritivas = df_all.groupby('Loja')[['Preco', 'Num Pecas']].describe()
 print(estatisticas_descritivas)
 
-# Verificar duplicatas
-print("Duplicatas antes da remoção:")
-print(df_all.duplicated().sum())
-
-# Remover duplicatas
-df_all = df_all.drop_duplicates()
-
-print("Duplicatas após a remoção:")
-print(df_all.duplicated().sum())
+# Apresentar os graficos na tela
+plt.show()
--- menor_preco.py
+++ menor_preco.py
@@ -2,13 +2,13 @@ import json
 
 # Carregar os arquivos JSON
 with open('legos_amazon_brasil.json', 'r', encoding='utf-8') as f:
-    amazon_data = json.load(f)
+    amazon_brasil_data = json.load(f)
 
 with open('legos_lego_store_brasil.json', 'r', encoding='utf-8') as f:
-    lego_store_data = json.load(f)
+    lego_store_brasil_data = json.load(f)
     
 with open('legos_bricklink.json', 'r', encoding='utf-8') as f:
-    briclink_data = json.load(f)
+    bricklink_data = json.load(f)
 
 # Funcao para extrair a primeira parte do ID
 def extrair_id_basico(produto_id):
@@ -29,8 +29,9 @@ def atualizar_num_pecas(menor_produto, novo_produto):
 menor_preco_dict = {}
 
 # Funcao para processar produtos de uma lista de dados
-def processar_produtos(data):
+def processar_produtos(data, valor):
     for produto in data:
+        produto.update({'Loja': valor})
         produto_id = extrair_id_basico(produto.get('ID'))
         if len(str(produto_id)) > 3:
             preco = produto.get('Preco')
@@ -44,9 +45,9 @@ def processar_produtos(data):
                         menor_preco_dict[produto_id] = atualizar_num_pecas(menor_preco_dict[produto_id], produto)
 
 # Processar os produtos de cada fonte de dados
-processar_produtos(amazon_data)
-processar_produtos(lego_store_data)
-processar_produtos(briclink_data)
+processar_produtos(amazon_brasil_data, 'Amazon Brasil')
+processar_produtos(lego_store_brasil_data, 'Lego Store Brasil')
+processar_produtos(bricklink_data, 'Bricklink')
 
 # Converter o dicionário em uma lista para salvar em JSON
 menor_preco_list = list(menor_preco_dict.values())
